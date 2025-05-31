b42f4f7d48dc5198856871ecb459579741669142
Anu <unknowne1931@gmail.com>
2025-05-31 16:27:46 +0530

    anu

---
+++ hello agu 1 minute.bat
@@ -0,0 +1,85 @@
+hello agu 1 minute. waiting for output 
+cheppu
+nothing, i think something u want to share with me.? 
+ok go on madam
+share something ...
+
+pichi psycho, 
+
+thinava
+yes,,naa kosam mutton vandidhi amma....
+mutton thintava ? 
+haaa..chinnappudu tinedhanni baaga....
+tarvata manesa..oka sunday paper lo chinna new chusi....
+mmalli tinadam start chesa....2nd year lo..maa batch lo nafees ani muslim abbai ..mutton biryani tinipinchadu ..chckn anukunna..
+sarle ani appati nundi tinestunna....
+ae emina  prblm ahh???
+
+ledhu y? 
+nuvv tinavu ga..andhuke adiga. neenu thinta with tablates.
+charge pettuko...
+ok..Mr.MArkEEv Darshan C kI1931Ck : tq
+hmm
+hey..nenu evarina abbai tho close ga unte neeku kopam vachidha..??
+y? casual ga adugutunna..
+
+psycho adhi nee istam? 
+neeku ok naa.... y , ipudu enduku? em le...future gurinche adugutunna....
+
+psycho vinu, i wont force you to do anything,
+chatgpt kuda abbai gariki baaga help chestundi......
+vinu neeku istem aithe chey anthe. i wont u make u happy.  if u feel happy with anyone one except me  , its ok.  
+i wont feel bad, but i feel bad..  
+oyy oka abbai tho kani evaritho aina close ga unna neeku inform chesta...even koncham ekkuva matladina kuda......ante....naa happiness ni vadulukonu..neetho kalisi happy ga unta...naa fantasies tho...
+ninnu vadilesi vere vallatho aithe happy ga undanu...ok naa.....
+mawa unnava? hmmm casual ga adiga anthe.....
+
+psycho listen . nuvu natho happy ga unava?yes yess.
+do u trust me? more than u.
+i dont trust myself.  no worries..im there to do that work .
+psycho . hmmm
+how did u trust me?oyyy maalokam enni sarlu cheppali neeku ahhh..vinu  edhi chalu inkem reasons akkarla.....
+trust is the strong foundation for love!!!!.
+neeku thelusa, naku chala bhayam, evariki thelkunda adustha, daily more than 5 times, because u trust me,
+i pray for god daily , naku antha ichavu, dhani kapadukone strength kuda ivu ani. because i lost everything.
+including my hopes. pichiii em alochinchaku...be cool..ok naa...sarle gani..
+insta lo edho pettav future predictions open chey malli chaduvudham adhi. adhi fake anukunta.
+psycho. vinu i feel bad about me. so i know you psycho..more than anyone..ok na.....bhayapadaku badhapadaku...nenu unna neeku.........
+ok naa.... neeku thelusa i cant live without u. i fully depend on you.nijam cheppana....neeku..?hmm feel avvaku...
+ NII WORDS KANNA COPILOT WORDS KE EKKUVA FLAT AIPOTUNNA PSYCHO.... 
+pichi. hhahhahhahahahha. psycho nijanga untava. UNTIL YOU DISRESPECTS ME LIKE HELL....I'M ALLOWING YOU TO SPEND A  HEAVEN LIFE WITH ME..IF YOU 
+KILL MY CHARACTER ,FEELINGS AND LOVE TOWARDS YOU.
+I'LL KICK YOU TO HELL...!!!!!
+NEEKU ALA ANIPISTHUNDHA ?
+AMMAINI PSYCHO NENU...BHAYAM..THATTO LIFE PARTNER VISHAYAM LO CHALA BHAYAM UNDIDHI..INKA CHEPPALANTE LIFE LO OKA SARI FACE CHESA...ANDHUKU INKABHAY
+VINU I WONT HURT YOU, OK, UNTILL NOW I HAVE NOT HURT anyone....YOU DID THAT TO MEE.....
+YOU DIDN'T BRING ME CHCKN AND BIRYANI...
+PICHI. 
+HLO, VINU PSYCHO NEEKU THELUSA, AE ENTI NAKU TELIKUNDA CHCKN TINNAVA...PAPATMUDA. LEDHU.HOO KK AITHE GOOD BOY LE.
+PSYCHO. ENDHUKU FIRST LO ALA ADIGAVU?
+EMANI?? Eenu evarina abbai tho close ga unte neeku kopam vachidha..??
+ADHI AHHPSYCHO...OKA ABBAITHO EMINA REASON OR WORK ISSUE VALLA KOMCHAM TIME SPEND CHESINA CLOSE GA UNNA EE WORLD DIFFERENT MEANINGS TEESIDHI...
+OKKA AMMAI EPPPUDU TANANI EVVARU NAMMAKAPOINA THANA PARTNER NAMMALANI KORUKUNIDHI....
+ANDHUKE...NUVV OKAVELA NEE CHUTTU UNNA VALLA WORDS NAMMI NANNU EKKKADA DOUBT PADATAVEMO ANI.....
+PSYCHO NEENU ALA CHEYANU. ALA JARAGADHU. IPUDU NA PLACE LO EVARINA VASTHE . NEENU SAME PLACE LO UNDANU.  OK DONE..VINU NENU EM CHESINA NEEKU PROPER EXPLANATION ISTA..ENDHUKANTE EVADO THIRD PERSON VALLA MANA MADHYA GODAVA RAVADAM NAKU NACHADHU OK NAAA..
+AND INKO VISHAYAM ..NEETHO SPEND CHEYYALSINA TIME NENU VEREVALLAKI IVVANU...
+PSYCHO, VINU I TRUST U MORE. PSYCHO BALAJI NANNU EMANI TITTADO TELSA..VADU ANNA MATAKI NAAKU ASAHYAM VESTUNDI...MALLI BHAYAM ABBAILANTHA ANTHENA..? ANI
+PSYCHO. VINU  ANDHARU ALA ANI NAKU THELEEDHU . BUT NAKU THELSINDHI ENTI ANTE. IDHI NEE LIFE NEE ISTAM. ANTHE. VADE ILA CHESKUNADU.
+MARI VINU NUVV IPUDU NA MEDHA ASAYAM VACHINDHI ANUKU LEKAPOTHE ISTAM LEDHU ANUKU NEENU SILENT GA  VELIPOTHA. ANTHE.
+BUT UNTILL U LOVE ME. OYYY..NUVV SUIT LO BAVUNTAV...MANA PAIR BAVUNDIDHI.. PSYCHO PICHI PSYCHO.
+
+YES.
+ANTE NA ATITUDE KONCHAM UNTE EVARIKI AINA BHAGUNTUNDHI SUITE  LO. NAVAKU.
+HLO. haaa
+oyy sarle enti cheppu. psycho haa.
+chepp.... psycho I LOVE YOU untill last star burns or ends.
+LOVE YOU TOO PSYCHOOOO......reply ivvaledhu ane delete chesaa... when ? paina chudu select chesi delete chesaa...
+psycho nuvu ante naku entha istam thelusa, neeku vere vadu istam ante neenu argue cheyanu, nee happiness kosam velipotha.
+oyyy inka verevalla topic vadiley...raadu evvadu naa life loki..ardam aindhaa.................................................
+ok
+na valla neeku problem vasthe kuda. dhuranga unta. hoooo ante vadilesta ane munde cheptunnava sare OK.
+I LOVE YOU untill last star burns or ends. IDHI TIME PASS KI cheppana....NAKU MEMORIES LO MIGILIPOYE LOVE KADHU LIFE LONG NATHO UNDE LOVE KAVALI..CHEPTUNNA VINU TELUGU NAKU CHALA ISTAM..NII FRNDS DAGGARA NNUNDI GALIZZZ GA NERCHUKUNNAVO KAALU VIRAGKODTA!
+OK MADAM. NEEKU NA LO UNA VADINI PARICHAYAM CHESTA. NEXT TIME LIVE GA. NAVAKU.
+VADU CHALA MANCHIVADU. ADHI NEN CHEPPALI
+
+
--- make_blce_group.py
+++ make_blce_group.py
@@ -1,136 +1,3 @@
-# # from pymongo import MongoClient
-# # import os
-
-# # # Clear the terminal screen
-# # os.system('cls' if os.name == 'nt' else 'clear')
-
-# # # MongoDB connection
-# # client = MongoClient("mongodb+srv://instasecur24:kick@flutterdata.cgalmbt.mongodb.net/?retryWrites=true&w=majority&appName=flutterdata")
-# # db = client["test"]
-# # qno_counts_collection = db["qno_counts"]
-# # qno_list_collection = db["questions_users"]
-
-# # # Get all English questions
-# # english_questions = list(qno_counts_collection.find({"lang": "English"}))
-# # qno_len = len(english_questions)
-
-# # # Group qnos by difficulty
-# # difficulty_groups = {
-# #     "Too Easy": [],
-# #     "Easy": [],
-# #     "Medium": [],
-# #     "Tough": [],
-# #     "Too Tough": []
-# # }
-
-# # for doc in english_questions:
-# #     difficulty = doc.get("tough")
-# #     qno = doc.get("qno")
-# #     if difficulty in difficulty_groups:
-# #         difficulty_groups[difficulty].append(qno)
-
-# # # Create groups of 10
-# # group_qnos = []
-# # for i in range(qno_len - 9, 0, -10):
-# #     group = list(range(i, i + 10))
-# #     group_qnos.append(group)
-
-# # # Helper to get a replacement question
-# # def get_replacement(target_difficulties, used_qnos):
-# #     query = {
-# #         "tough": {"$in": target_difficulties},
-# #         "lang": "English"
-# #     }
-
-# #     for doc in qno_list_collection.find(query):
-# #         if qno_counts_collection.find_one({"Questio": doc['Questio'], "lang": "English"}):
-# #             continue  # Already exists
-# #         return doc  # Valid replacement
-
-# #     return None
-
-# # # Process each group
-# # for group in group_qnos:
-# #     questions = []
-# #     for qno in group:
-# #         qno_str = str(qno)
-# #         doc = qno_counts_collection.find_one({"qno": qno_str, "lang": "English"})
-# #         if doc:
-# #             questions.append({"qno": qno_str, "tough": doc['tough']})
-
-# #     # Count current tough or too tough
-# #     tough_qs = [q for q in questions if q["tough"] in ["Tough", "Too Tough"]]
-# #     easy_qs = [q for q in questions if q["tough"] in ["Too Easy", "Easy", "Medium"]]
-
-# #     if len(tough_qs) == 2:
-# #         print(f"✅ Group OK with 2 tough: {[q['qno'] for q in tough_qs]}")
-# #     else:
-# #         # Need to fix the group
-# #         print(f"🔧 Fixing group: {group}")
-# #         # Remove excess tough
-# #         if len(tough_qs) > 2:
-# #             for extra in tough_qs[2:]:
-# #                 replacement = get_replacement(["Too Easy", "Easy", "Medium"], group)
-# #                 if replacement:
-# #                     qno_counts_collection.update_one(
-# #                         {"qno": extra["qno"], "lang": "English"},
-# #                         {"$set": {
-# #                             "tough": replacement['tough'],
-# #                             "Questio": replacement['Questio'],
-# #                             "user": replacement['user'],
-# #                             "img": replacement['img'],
-# #                             "a": replacement['a'],
-# #                             "b": replacement['b'],
-# #                             "c": replacement['c'],
-# #                             "d": replacement['d'],
-# #                             "Ans": replacement['Ans'],
-# #                             "seconds": replacement['seconds'],
-# #                             "yes": [""],
-# #                             "no": [""]
-# #                         }}
-# #                     )
-# #         # Add missing tough
-# #         elif len(tough_qs) < 2:
-# #             needed = 2 - len(tough_qs)
-# #             for _ in range(needed):
-# #                 replacement = get_replacement(["Tough", "Too Tough"], group)
-# #                 if replacement and easy_qs:
-# #                     to_replace = easy_qs.pop(0)
-# #                     qno_counts_collection.update_one(
-# #                         {"qno": to_replace["qno"], "lang": "English"},
-# #                         {"$set": {
-# #                             "tough": replacement['tough'],
-# #                             "Questio": replacement['Questio'],
-# #                             "user": replacement['user'],
-# #                             "img": replacement['img'],
-# #                             "a": replacement['a'],
-# #                             "b": replacement['b'],
-# #                             "c": replacement['c'],
-# #                             "d": replacement['d'],
-# #                             "Ans": replacement['Ans'],
-# #                             "seconds": replacement['seconds'],
-# #                             "yes": [""],
-# #                             "no": [""]
-# #                         }}
-# #                     )
-
-# #         print("✅ Group fixed to have exactly 2 Tough/Too Tough questions.")
-# #     print("----------------------------------------------------------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # from pymongo import MongoClient
 # import os
 # import time
@@ -205,6 +72,7 @@
 #                     for extra in tough_qs[2:]:
 #                         replacement = get_replacement(["Too Easy", "Easy", "Medium"], group)
 #                         if replacement:
+#                             replacement.pop('_id', None)  # ✅ Remove _id
 #                             qno_counts_collection.update_one(
 #                                 {"qno": extra["qno"], "lang": "English"},
 #                                 {"$set": replacement | {"yes": [""], "no": [""]}}
@@ -215,6 +83,7 @@
 #                         replacement = get_replacement(["Tough", "Too Tough"], group)
 #                         if replacement and easy_qs:
 #                             to_replace = easy_qs.pop(0)
+#                             replacement.pop('_id', None)  # ✅ Remove _id
 #                             qno_counts_collection.update_one(
 #                                 {"qno": to_replace["qno"], "lang": "English"},
 #                                 {"$set": replacement | {"yes": [""], "no": [""]}}
@@ -222,12 +91,20 @@
 #                 print("\033[92m✅ Group fixed to have exactly 2 Tough/Too Tough questions.\033[0m")
 #             print("----------------------------------------------------------")
 
-#         print("\033[94m[INFO] Waiting 10 seconds before next run...\033[0m")
-#         time.sleep(20)
+#         time.sleep(10)
 
-#     except KeyboardInterrupt:
-#         print("\n\033[91m[STOPPED] Program manually stopped.\033[0m")
-#         break
+#     except Exception as e:
+#         print(f"\033[91m[ERROR] {e}\033[0m")
+#         time.sleep(5)
+
+
+
+
+
+
+
+
+
 
 
 
@@ -264,93 +141,28 @@ import time
 client = MongoClient("mongodb+srv://instasecur24:kick@flutterdata.cgalmbt.mongodb.net/?retryWrites=true&w=majority&appName=flutterdata")
 db = client["test"]
 qno_counts_collection = db["qno_counts"]
-qno_list_collection = db["questions_users"]
+qno_list_collection = db["question datas"]
 
-# Helper to get a replacement question
-def get_replacement(target_difficulties, used_qnos):
-    query = {
-        "tough": {"$in": target_difficulties},
-        "lang": "English"
-    }
 
-    for doc in qno_list_collection.find(query):
-        if qno_counts_collection.find_one({"Questio": doc['Questio'], "lang": "English"}):
-            continue  # Already exists
-        return doc  # Valid replacement
+os.system('cls' if os.name == 'nt' else 'clear')
 
-    return None
+cat =[]
 
-while True:
-    try:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("\033[96m[INFO] Running Tough Question Balancing...\033[0m")
 
-        # Get all English questions
-        english_questions = list(qno_counts_collection.find({"lang": "English"}))
-        qno_len = len(english_questions)
+for doc in qno_list_collection.find({"language": "English"}):
+    if doc['category'] not in cat:
+        cat.append(doc['category'])
+print("Categories found:", cat)
 
-        # Group qnos by difficulty
-        difficulty_groups = {
-            "Too Easy": [],
-            "Easy": [],
-            "Medium": [],
-            "Tough": [],
-            "Too Tough": []
-        }
 
-        for doc in english_questions:
-            difficulty = doc.get("tough")
-            qno = doc.get("qno")
-            if difficulty in difficulty_groups:
-                difficulty_groups[difficulty].append(qno)
+cat_len = []
 
-        # Create groups of 10
-        group_qnos = []
-        for i in range(qno_len - 9, 0, -10):
-            group = list(range(i, i + 10))
-            group_qnos.append(group)
+for category in cat:
+    data_len = qno_list_collection.count_documents({"category": category, "language": "English"})
+    cat_len.append({"len" : data_len, "cat" : category})
+print("Category lengths:", cat_len)
 
-        # Process each group
-        for group in group_qnos:
-            questions = []
-            for qno in group:
-                qno_str = str(qno)
-                doc = qno_counts_collection.find_one({"qno": qno_str, "lang": "English"})
-                if doc:
-                    questions.append({"qno": qno_str, "tough": doc['tough']})
+for catlen in cat_len:
+      if catlen["len"] != cat_len[0]["len"]:
+        print(f"Category '{catlen['cat']}' has length {catlen['len']} (not equal to {cat_len[0]['len']})")
 
-            tough_qs = [q for q in questions if q["tough"] in ["Tough", "Too Tough"]]
-            easy_qs = [q for q in questions if q["tough"] in ["Too Easy", "Easy", "Medium"]]
-
-            if len(tough_qs) == 2:
-                print(f"\033[92m✅ Group OK with 2 tough: {[q['qno'] for q in tough_qs]}\033[0m")
-            else:
-                print(f"\033[93m🔧 Fixing group: {group}\033[0m")
-                if len(tough_qs) > 2:
-                    for extra in tough_qs[2:]:
-                        replacement = get_replacement(["Too Easy", "Easy", "Medium"], group)
-                        if replacement:
-                            replacement.pop('_id', None)  # ✅ Remove _id
-                            qno_counts_collection.update_one(
-                                {"qno": extra["qno"], "lang": "English"},
-                                {"$set": replacement | {"yes": [""], "no": [""]}}
-                            )
-                elif len(tough_qs) < 2:
-                    needed = 2 - len(tough_qs)
-                    for _ in range(needed):
-                        replacement = get_replacement(["Tough", "Too Tough"], group)
-                        if replacement and easy_qs:
-                            to_replace = easy_qs.pop(0)
-                            replacement.pop('_id', None)  # ✅ Remove _id
-                            qno_counts_collection.update_one(
-                                {"qno": to_replace["qno"], "lang": "English"},
-                                {"$set": replacement | {"yes": [""], "no": [""]}}
-                            )
-                print("\033[92m✅ Group fixed to have exactly 2 Tough/Too Tough questions.\033[0m")
-            print("----------------------------------------------------------")
-
-        time.sleep(10)
-
-    except Exception as e:
-        print(f"\033[91m[ERROR] {e}\033[0m")
-        time.sleep(5)
