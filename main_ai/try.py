a6e3e358d1022c387cec5c0309ffa60fe72d1dfe
Anu <unknowne1931@gmail.com>
2025-06-25 01:06:39 +0530

    anu

--- main_ai/try.py
+++ main_ai/try.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 from collections import defaultdict
 
 
+os.system('cls')
+
+
 MONGODB_URI = os.getenv(
     "MONGODB_URI",
     "mongodb+srv://instasecur24:kick@flutterdata.cgalmbt.mongodb.net/?retryWrites=true&w=majority&appName=flutterdata"
@@ -16,5 +19,67 @@ start_stop_db = db["start_stops"]
 qns_stored = db["question datas"]
 
 
+expected_qno = 10
+
+# ✅ System control function
+def stop_start(new_status):
+    start_stop = start_stop_db.find_one({})
+    if not start_stop:
+        start_stop_db.insert_one({"Status": new_status})
+        print(f"\033[93m⚠️ Created Status Document with value: {new_status}\033[0m")
+        return
+
+    current_status = start_stop.get("Status", "on")
+
+    if current_status.lower() != new_status.lower():
+        start_stop_db.update_one({"_id": start_stop["_id"]}, {"$set": {"Status": new_status}})
+        if new_status != "on":
+            print(f"\033[91m⛔ Game stopped. Status changed to: {new_status}\033[0m")
+        else:
+            print("\033[92m✅ Game is running...\033[0m")
+    else:
+        print("\033[91m⛔ System is stopped. Please start it to continue.\033[0m")
+
+stop_start('off')
+
+main_qns.delete_many({})
+
+data_qns = qns_stored.find({})
+for i, dat in enumerate(data_qns,1):
+    print(f"Qno : {i} ----> {dat['difficulty']}")
+    ans = ""
+    if dat['answer'] == dat['a']:
+        ans = 'a'
+    elif dat['answer'] == dat['b']:
+        ans = 'b'
+    elif dat['answer'] == dat['c']:
+        ans = 'c'
+    else:
+        ans = 'd'
+
+    main_qns.insert_one({
+        "Time": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "ID" : dat['_id'],
+        "user" : "staWro",
+        "img" : dat['image'],
+        "Questio" : dat['question'],
+        "qno" : str(i),
+        "a" : dat['a'],
+        "b" : dat['b'],
+        "c" : dat['c'],
+        "d" : dat['d'],
+        "Ans" : ans,
+        "lang" : dat['language'],
+        "tough" : dat['difficulty'],
+        "seconds" :dat['seconds'],
+        "sub_lang" : dat['category'],
+        "yes" : [''],
+        "no" : ['']
+    })
+
+    print(f'Qno {i} Posted ok ')
+
+
+stop_start('on')
 
 
